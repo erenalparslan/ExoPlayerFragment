4a2182a2d26c1dbb49178e3876cee95ad173c0a2
erenalparslan <erenalparslan52@gmail.com>
2023-05-07 18:31:06 +0300

    oriantation and theme

--- app/src/main/java/com/erenalparslan/octopusclone/Play.kt
+++ app/src/main/java/com/erenalparslan/octopusclone/Play.kt
@@ -1,33 +1,24 @@
 package com.erenalparslan.octopusclone
 
-import android.app.AlertDialog
-import android.content.Context
+
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.inflate
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
 
 import com.erenalparslan.octopusclone.databinding.FragmentPlayBinding
 import com.google.android.exoplayer2.*
-import com.google.android.exoplayer2.source.TrackGroupArray
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.trackselection.TrackSelectionParameters
 import com.google.android.exoplayer2.ui.StyledPlayerView
-import com.google.android.exoplayer2.ui.TrackSelectionView
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.android.exoplayer2.util.EventLogger
 
 
-private var currentView: View? = null
-
 class Play : Fragment() {
-
+    private var currentView: View? = null
     private val KEY_TRACK_SELECTION_PARAMETERS = "track_selection_parameters"
-    private val KEY_SERVER_SIDE_ADS_LOADER_STATE = "server_side_ads_loader_state"
     private val KEY_ITEM_INDEX = "item_index"
     private val KEY_POSITION = "position"
     private val KEY_AUTO_PLAY = "auto_play"
@@ -38,13 +29,8 @@ class Play : Fragment() {
     private var playWhenReady = true
     private var player: ExoPlayer? = null
     private var playerView: StyledPlayerView? = null
-    var isVideoPlaying = false
-    var isVideoPlayingEnd = false
     private var fullscreenButton: ImageView? = null
-    private val selectTracksButton: Button? = null
 
-    // private var exoPlay: ImageButton? = null
-    // private var exoPause: ImageButton? = null
     private var exoPlay: ImageView? = null
     private var exoPause: ImageView? = null
     private var exoRestart: ImageView? = null
@@ -54,20 +40,13 @@ class Play : Fragment() {
     private var exoPosition: TextView? = null
     private var exoDuration: TextView? = null
 
-    private var startTime: Long? = 0
     private var lastSeenTracks: Tracks? = null
-    private var lastSeenTrackGroupArray: TrackGroupArray? = null
-    private var fullscreen = false
     var isShowingTrackSelectionDialog: Boolean = false
-    private val trackSelector: DefaultTrackSelector? = null
     private var trackSelectionParameters: TrackSelectionParameters? = null
 
     private var startAutoPlay = false
     private var startItemIndex = 0
     private var startPosition: Long = 0
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -80,22 +59,22 @@ class Play : Fragment() {
     ): View? {
 
         // Inflate the layout for this fragment
-        currentView= inflater.inflate(R.layout.fragment_play, container, false)
+        currentView = inflater.inflate(R.layout.fragment_play, container, false)
         return currentView
 
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-        println(currentView)
+   /*     println(currentView)
         val inflater = LayoutInflater.from(requireContext())
-         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-           currentView= inflater.inflate(R.layout.fragment_play_land, null)
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            currentView = inflater.inflate(R.layout.fragment_play, null)
+            //  activity?.setContentView(currentView)
         } else {
 
-        }
+        }*/
     }
-
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -120,11 +99,6 @@ class Play : Fragment() {
     }
 
 
-    companion object {
-        var URL =
-            "https://dxnqhjm6zg0n4.cloudfront.net/file_library/videos/vod_non_drm_ios/74762/active_forum_-_final_videos/1655192575290_184433_VOD.m3u8"
-    }
-
     private fun preparePlayer() {
         //  exoPlayer = ExoPlayer.Builder(this).build()
 
@@ -146,26 +120,22 @@ class Play : Fragment() {
             //   .setTrackSelector(trackSelector)
             .build()
         exoPlayer!!.trackSelectionParameters = trackSelectionParameters!!
-        //exoPlayer!!.addListener(playerEventListener)
         exoPlayer!!.addListener(PlayerEventListener())
         exoPlayer!!.addAnalyticsListener(EventLogger())
         exoPlayer?.playWhenReady = true
         fragmentPlayBinding?.playerView?.player = exoPlayer
 
-        val defaultHttpDataSourceFactory = DefaultHttpDataSource.Factory()
 
         val mediaItem = MediaItem.fromUri(URL)
-        //   val mediaSource = HlsMediaSource.Factory(defaultHttpDataSourceFactory).createMediaSource(mediaItem)
 
         exoPlayer?.addMediaItem(mediaItem)
-        //   exoPlayer?.setMediaSource(mediaSource)
         exoPlayer?.seekTo(playbackPosition)
         exoPlayer?.playWhenReady = playWhenReady
         exoPlayer?.prepare()
         //  playerView?.player = player
 
 
-        fun showQualitySelector() {
+/*        fun showQualitySelector() {
             val trackSelector = exoPlayer?.trackSelector
             if (trackSelector != null) {
                 val trackSelectionView = TrackSelectionView(requireContext())
@@ -179,7 +149,7 @@ class Play : Fragment() {
                     .setNegativeButton(android.R.string.cancel, null)
                 builder.show()
             }
-        }
+        }*/
 
         ivVideoQuality?.setOnClickListener {
 
@@ -223,9 +193,15 @@ class Play : Fragment() {
         }
     }
 
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+
+
+
+
         updateTrackSelectorParameters()
+
 
         outState.putBundle(
             KEY_TRACK_SELECTION_PARAMETERS,
@@ -246,20 +222,6 @@ class Play : Fragment() {
         // saveServerSideAdsLoaderState(outState)
     }
 
-    // User controls
-    private fun updateButtonVisibility() {
-        fragmentPlayBinding?.selectTracksButton?.isEnabled =
-            player != null && TrackSelectionDialog.willHaveContent(player)
-    }
-
-    private fun showToast(messageId: Int) {
-        showToast(getString(messageId))
-    }
-
-    private fun showToast(message: String) {
-        Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
-    }
-
 
     override fun onStop() {
         super.onStop()
@@ -276,7 +238,10 @@ class Play : Fragment() {
         releasePlayer()
     }
 
-
+    companion object {
+        var URL =
+            "https://dxnqhjm6zg0n4.cloudfront.net/file_library/videos/vod_non_drm_ios/74762/active_forum_-_final_videos/1655192575290_184433_VOD.m3u8"
+    }
 
 
 }
